d7e7ba46c407bc1be19a781762763e8d81f96c83
richmondvan <evan.ty.zheng@gmail.com>
2020-05-27 18:50:44 -0400

    - Created initial layout

--- src/main/java/stl/threebodysimulation/MainApp.java
+++ src/main/java/stl/threebodysimulation/MainApp.java
@@ -15,7 +15,7 @@ public class MainApp extends Application {
 
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
-
+        stage.setResizable(false);
         stage.setTitle("JavaFX and Gradle");
         stage.setScene(scene);
         stage.show();
