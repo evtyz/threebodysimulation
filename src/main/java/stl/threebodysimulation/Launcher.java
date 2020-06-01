2e4fe40d723475a8deedd2a9692a05dbdf3c0c40
richmondvan <evan.ty.zheng@gmail.com>
2020-05-31 23:19:03 -0400

    - Build settings completed

---
+++ src/main/java/stl/threebodysimulation/Launcher.java
@@ -0,0 +1,9 @@
+package stl.threebodysimulation;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(MainApp.class, args);
+    }
+}
